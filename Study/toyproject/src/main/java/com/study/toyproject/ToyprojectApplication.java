a7935d434fc48e091ea40a57c0b8ce36e87d9d6c
jangyoohyun <nikenycany46@naver.com>
2022-02-18 19:58:11 +0900

    2022-02-18

---
+++ Study/toyproject/src/main/java/com/study/toyproject/ToyprojectApplication.java
@@ -0,0 +1,15 @@
+package com.study.toyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectApplication.class, args);
+	}
+
+}
---
+++ Study/toyproject/src/main/java/com/study/toyproject/util/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.study.toyproject.util;
+
+import java.time.LocalDate;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	private LocalDate createDate;
+	
+}
